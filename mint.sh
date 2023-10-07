6a918bb44d4ef5c157603c96f8a09336515d4eb6
Utah8O <147143937+Utah8O@users.noreply.github.com>
2023-10-08 00:51:58 +0300

    Add files via upload

---
+++ mint.sh
@@ -0,0 +1,10 @@
+APPNAME="token2121"
+PRIVATEKEY="APrivateKey1zkp6cuiGs7GL6qy8U8pv6DVYhasg5E1vZ2TEuWuabGWkec5"
+
+RECORD="{
+  owner: aleo1dq3penpukclwxultp0kwahnr0j95fsvnwfd5sjzckyqj9mq6lg9sp7ke4x.private,
+  microcredits: 50000000u64.private,
+  _nonce: 5372051879012195041848680270446571484541298501248666503372405354256229274885group.public
+}"
+
+snarkos developer execute "${APPNAME}.aleo" "mint" 100u32 --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --broadcoast "https://vm.aleo.org/api/testnet3/transaction/broadcoast" --fee 1000000 --record "${RECORD}"
